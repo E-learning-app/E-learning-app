93ed37ed938c0e3aca501abbcc9b36be9d83b83d
Adam <abejaoui90@gmail.com>
2023-10-10 15:15:03 +0100

    added signup and signin

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
-import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Component/Login';
+import Signup from './Component/Signup';
+import Home from './Component/Home';
 
 function App() {
   return (
-    <MDBContainer fluid>
-      <div
-        className='d-flex justify-content-center align-items-center'
-        style={{ height: '100vh' }}
-      >
-        <div className='text-center'>
-          <img
-            className='mb-4'
-            src='https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'
-            style={{ width: 250, height: 90 }}
-          />
-          <h5 className='mb-3'>
-            Thank you for using our product. We're glad you're with us.
-          </h5>
-          <p className='mb-3'>MDB Team</p>
-          <MDBBtn
-            tag='a'
-            href='https://mdbootstrap.com/docs/standard/getting-started/'
-            target='_blank'
-            role='button'
-          >
-            Start MDB tutorial
-          </MDBBtn>
-        </div>
-      </div>
-    </MDBContainer>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/home" element={<Home/>} />
+    </Routes>
+    
+    </BrowserRouter>
   );
 }
 
