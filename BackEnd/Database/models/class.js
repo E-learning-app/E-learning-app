7ff7d77df1a51c106d87dffc7f12d9e51558ae96
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-10-10 17:33:45 +0200

    done

---
+++ BackEnd/Database/models/class.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Class = sequelize.define("class", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Class;
+};
