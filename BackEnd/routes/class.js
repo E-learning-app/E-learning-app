bde4d7e172e26dd1a68c04fccc03100317fcc2da
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-10-11 17:20:47 +0200

    implemented cloudinary correctly from client only

--- BackEnd/routes/class.js
+++ BackEnd/routes/class.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const cloudinary = require("./utils/cloudinary");
 
 const {
   getAllClasses,
--- Client/src/App.js
+++ Client/src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Component/Login';
-import Signup from './Component/Signup';
-import Home from './Component/Home';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Component/Login";
+import Signup from "./Component/Signup";
+import Home from "./Component/Home";
+import AddClass from "./Component/AddClass";
 
 function App() {
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/signup" element={<Signup/>} />
-      <Route path="/home" element={<Home/>} />
-    </Routes>
-    </BrowserRouter>
-  );
+  return <AddClass />;
 }
 
 export default App;
