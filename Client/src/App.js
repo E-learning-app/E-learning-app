1e1774f208cc7be1bb41740de8a3ffd69bc8c782
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-10-11 20:06:43 +0200

    push

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,7 +6,14 @@ import Home from "./Component/Home";
 import AddClass from "./Component/AddClass";
 
 function App() {
-  return <AddClass />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
