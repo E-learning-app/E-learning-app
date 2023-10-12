1cc44de6569b0360d2ea620dbad121d82f5fc1cf
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-10-12 11:05:41 +0200

    pulled from main

--- Client/src/Component/Signup.jsx
+++ Client/src/Component/Signup.jsx
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios'
 function Signup() {
-  const [inputs,setInputs]=useState({})
+  const [inputs, setInputs] = useState({})
   console.log(inputs)
-const handleChange =(e)=>{
-  e.preventDefault()
-  const name = e.target.name;
-  const value = e.target.value;
-  setInputs(values => ({...values, [name]: value}))
+  const handleChange = (e) => {
+    e.preventDefault()
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
 
-}
+  }
 
-const handleSubmit =(e)=>{
-  e.preventDefault()
-  axios.post("http://localhost:3000/User/addUser",inputs)
-  .then((response)=>{
-    console.log("user added successfully")
-    console.log(response.data)
-  }).catch((err)=>{
-    console.log(err)
-  })
-}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post("http://localhost:3000/User/addUser", inputs)
+      .then((response) => {
+        console.log("user added successfully")
+        console.log(response.data)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
 
   return (
     <section className="vh-100" style={{ backgroundColor: '#D3D3D3' }}>
@@ -35,7 +35,7 @@ const handleSubmit =(e)=>{
                     src="https://img.freepik.com/photos-gratuite/jeune-homme-etudiant-dans-bibliotheque-aide-ordinateur-portable_23-2149285400.jpg?size=626&ext=jpg&ga=GA1.2.1345089941.1696063536&semt=sph"
                     alt="signup form"
                     className="img-fluid "
-                    style={{ borderRadius: '5rem 0 0 5rem', height:"100%"}}
+                    style={{ borderRadius: '5rem 0 0 5rem', height: "100%" }}
                   />
                 </div>
                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
@@ -73,11 +73,11 @@ const handleSubmit =(e)=>{
                         </label>
                       </div>
                       <div className="pt-1 mb-4">
-                      <Link to="/"><button className="btn btn-dark btn-lg btn-block" type="submit" style={{ backgroundColor: '#ff6219' }} onClick={handleSubmit} > Signup
+                        <Link to="/login"><button className="btn btn-dark btn-lg btn-block" type="submit" style={{ backgroundColor: '#ff6219' }} onClick={handleSubmit} > Signup
                         </button></Link>
                       </div>
                       <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>
-                      Already have a account? <Link to="/"><span href="/" style={{ color: '#ff6219' }} >Login here</span></Link>
+                        Already have a account? <Link to="/login"><span style={{ color: '#ff6219' }} >Login here</span></Link>
                       </p>
 
                     </form>
