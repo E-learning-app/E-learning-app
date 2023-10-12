24201f9c053f3482e6b3613b1d26009a209b12ea
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-10-12 20:02:37 +0200

    learning about pdf upload

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,24 +3,19 @@ const app = express();
 const cors = require("cors");
 const db = require("./Database/index");
 const classRouter = require("./routes/class");
-const UserRouter =require("./routes/user")
+const UserRouter = require("./routes/user");
 const courseRouter = require("./routes/course");
-const testAuthentication = require("./routes/testAuthentication")
-require("dotenv").config()
-
-
-
+const testAuthentication = require("./routes/testAuthentication");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/classess", classRouter);
-app.use("/User",UserRouter)
+app.use("/User", UserRouter);
 app.use("/courses", courseRouter);
-
 
 app.listen(3000, () => {
   console.log("listen on port 3000 ");
 });
-
